785b571bcde065afefd4b81e82833aa644dbeeff
Carl Lundin <clundin@google.com>
2025-02-12 15:56:34 -0800

    Working on building boot image

--- ci-tools/fpga-image/build.sh
+++ ci-tools/fpga-image/build.sh
@@ -9,10 +9,9 @@ set -e
 set -x
 
 mkdir -p out
-
-SYSTEM_BOOT_SHA256="714cc0b12607c476672f569b3f996ce8b3446bd05b30bffcd1c772c483923098"
+SYSTEM_BOOT_SHA256="5a22eac02deb38825ed5df260e394753f440d546a34b9ed30ac096eb3aed2eb5"
 if ! (echo "${SYSTEM_BOOT_SHA256} out/system-boot.tar.gz" | sha256sum -c); then
-  curl -o out/system-boot.tar.gz https://people.canonical.com/~platform/images/xilinx/zcu-ubuntu-22.04/iot-limerick-zcu-classic-desktop-2204-x05-2-20221123-58-system-boot.tar.gz
+  curl -o out/system-boot.tar.gz https://people.canonical.com/~platform/images/xilinx/versal-ubuntu-22.04/iot-limerick-versal-classic-server-2204-x02-20230315-48-system-boot.tar.gz
   if ! (echo "${SYSTEM_BOOT_SHA256} out/system-boot.tar.gz" | sha256sum -c); then
     echo "Downloaded system-boot file did not match expected sha256sum".
     exit 1
@@ -20,70 +19,79 @@ if ! (echo "${SYSTEM_BOOT_SHA256} out/system-boot.tar.gz" | sha256sum -c); then
 fi
 
 # Build the rootfs
-if [[ -z "${SKIP_DEBOOTSTRAP}" ]]; then
-  (rm -rf out/rootfs || true)
-  mkdir -p out/rootfs
-  debootstrap --include git,curl,ca-certificates,locales,libicu72,sudo,vmtouch,fping,rdnssd,dbus,systemd-timesyncd,libboost-regex1.74.0,openocd,gdb-multiarch --arch arm64 --foreign bookworm out/rootfs
-  chroot out/rootfs /debootstrap/debootstrap --second-stage
-  chroot out/rootfs useradd runner --shell /bin/bash --create-home
-
-  # Jobs need to act as root to install an FPGA bitstream. We don't care
-  # if they mess up the rootfs because it's going to be re-flashed after the job
-  # terminates anyways.
-  echo "runner ALL=(ALL) NOPASSWD:ALL" > out/rootfs/etc/sudoers.d/runner
-
-  chroot out/rootfs mkdir /mnt/root_base
-  chroot out/rootfs mkdir /mnt/root_overlay
-  chroot out/rootfs mkdir /mnt/new_root
-
-  chroot out/rootfs bash -c 'echo caliptra-fpga > /etc/hostname'
-  chroot out/rootfs bash -c 'echo auto end0 > /etc/network/interfaces'
-  chroot out/rootfs bash -c 'echo allow-hotplug end0 >> /etc/network/interfaces'
-  chroot out/rootfs bash -c 'echo iface end0 inet6 auto >> /etc/network/interfaces'
-  chroot out/rootfs bash -c 'echo nameserver 2001:4860:4860::6464 > /etc/resolv.conf'
-  chroot out/rootfs bash -c 'echo nameserver 2001:4860:4860::64 >> /etc/resolv.conf'
-  chroot out/rootfs bash -c 'echo kernel.softlockup_panic = 1 >> /etc/sysctl.conf'
-  chroot out/rootfs bash -c 'echo kernel.softlockup_all_cpu_backtrace = 1 >> /etc/sysctl.conf'
-  chroot out/rootfs bash -c 'echo kernel.panic_print = 127 >> /etc/sysctl.conf'
-  chroot out/rootfs bash -c 'echo kernel.sysrq = 1 >> /etc/sysctl.conf'
-  chroot out/rootfs bash -c 'echo "[Time]" > /etc/systemd/timesyncd.conf'
-  chroot out/rootfs bash -c 'echo "NTP=time.google.com" >> /etc/systemd/timesyncd.conf'
-
-  # Comment this line out if you don't trust folks with physical access to the
-  # uart
-  # chroot out/rootfs bash -c 'echo root:password | chpasswd'
-  #
-
-  echo Retrieving latest GHA runner version
-  RUNNER_VERSION="$(curl https://api.github.com/repos/actions/runner/releases/latest | jq -r '.tag_name[1:]')"
-  echo Using runner version ${RUNNER_VERSION}
-  trap - EXIT
-  (cd out/rootfs/home/runner && curl -O -L "https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz")
-  chroot out/rootfs bash -c "su runner -c \"cd /home/runner && tar xvzf actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz && rm -f actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz\""
+ROOT_FS_SHA256="198281b2d2541a63b6ff0f52d65a60b7715492747efe60576c80760813af823a"
+if ! (echo "${ROOT_FS_SHA256} out/rootfs.tar.gz" | sha256sum -c); then
+  curl -o out/rootfs.tar.gz "https://people.canonical.com/~platform/images/xilinx/versal-ubuntu-22.04/iot-limerick-versal-classic-server-2204-x02-20230315-48-rootfs.tar.gz"
+  if ! (echo "${ROOT_FS_SHA256} out/rootfs.tar.gz" | sha256sum -c); then
+    echo "Downloaded rootfs file did not match expected sha256sum".
+    exit 1
+  fi
 fi
 
-su $SUDO_USER -c "
-  CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=\"aarch64-linux-gnu-gcc\" \
-  CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS=\"-C link-arg=--sysroot=$PWD/out/rootfs\" \
-  ~/.cargo/bin/cargo install cargo-nextest@0.9.64 \
-    --locked \
-    --no-default-features \
-    --features=default-no-update \
-    --target=aarch64-unknown-linux-gnu \
-    --root /tmp/cargo-nextest"
+(rm -rf out/rootfs || true)
+mkdir -p out/rootfs
+tar xvzf out/rootfs.tar.gz -C out/rootfs
 
-cp /tmp/cargo-nextest/bin/cargo-nextest out/rootfs/usr/bin/
 
-chroot out/rootfs bash -c 'echo ::1 caliptra-fpga >> /etc/hosts'
-cp startup-script.sh out/rootfs/usr/bin/
-chroot out/rootfs chmod 755 /usr/bin/startup-script.sh
-cp startup-script.service out/rootfs/etc/systemd/system/
-chroot out/rootfs systemctl enable startup-script.service
+# if [[ -z "${SKIP_DEBOOTSTRAP}" ]]; then
+#   (rm -rf out/rootfs || true)
+#   mkdir -p out/rootfs
+#   debootstrap --include git,curl,ca-certificates,locales,libicu72,sudo,vmtouch,fping,rdnssd,dbus,systemd-timesyncd,libboost-regex1.74.0,openocd,gdb-multiarch --arch arm64 --foreign bookworm out/rootfs
+#   chroot out/rootfs /debootstrap/debootstrap --second-stage
+#   chroot out/rootfs useradd runner --shell /bin/bash --create-home
+#
+#   # Jobs need to act as root to install an FPGA bitstream. We don't care
+#   # if they mess up the rootfs because it's going to be re-flashed after the job
+#   # terminates anyways.
+#   echo "runner ALL=(ALL) NOPASSWD:ALL" > out/rootfs/etc/sudoers.d/runner
+#
+#   chroot out/rootfs mkdir /mnt/root_base
+#   chroot out/rootfs mkdir /mnt/root_overlay
+#   chroot out/rootfs mkdir /mnt/new_root
+#
+#   chroot out/rootfs bash -c 'echo caliptra-fpga > /etc/hostname'
+#   chroot out/rootfs bash -c 'echo auto end0 > /etc/network/interfaces'
+#   chroot out/rootfs bash -c 'echo allow-hotplug end0 >> /etc/network/interfaces'
+#   chroot out/rootfs bash -c 'echo iface end0 inet6 auto >> /etc/network/interfaces'
+#   chroot out/rootfs bash -c 'echo nameserver 2001:4860:4860::6464 > /etc/resolv.conf'
+#   chroot out/rootfs bash -c 'echo nameserver 2001:4860:4860::64 >> /etc/resolv.conf'
+#   chroot out/rootfs bash -c 'echo kernel.softlockup_panic = 1 >> /etc/sysctl.conf'
+#   chroot out/rootfs bash -c 'echo kernel.softlockup_all_cpu_backtrace = 1 >> /etc/sysctl.conf'
+#   chroot out/rootfs bash -c 'echo kernel.panic_print = 127 >> /etc/sysctl.conf'
+#   chroot out/rootfs bash -c 'echo kernel.sysrq = 1 >> /etc/sysctl.conf'
+#   chroot out/rootfs bash -c 'echo "[Time]" > /etc/systemd/timesyncd.conf'
+#   chroot out/rootfs bash -c 'echo "NTP=time.google.com" >> /etc/systemd/timesyncd.conf'
+#
+#   # Comment this line out if you don't trust folks with physical access to the
+#   # uart
+#   # chroot out/rootfs bash -c 'echo root:password | chpasswd'
+#   #
+#
+#   echo Retrieving latest GHA runner version
+#   RUNNER_VERSION="$(curl https://api.github.com/repos/actions/runner/releases/latest | jq -r '.tag_name[1:]')"
+#   echo Using runner version ${RUNNER_VERSION}
+#   trap - EXIT
+#   (cd out/rootfs/home/runner && curl -O -L "https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz")
+#   chroot out/rootfs bash -c "su runner -c \"cd /home/runner && tar xvzf actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz && rm -f actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz\""
+#
+# su $SUDO_USER -c "
+#   CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=\"aarch64-linux-gnu-gcc\" \
+#   CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUSTFLAGS=\"-C link-arg=--sysroot=$PWD/out/rootfs\" \
+#   ~/.cargo/bin/cargo install cargo-nextest@0.9.64 \
+#     --locked \
+#     --no-default-features \
+#     --features=default-no-update \
+#     --target=aarch64-unknown-linux-gnu \
+#     --root /tmp/cargo-nextest"
+#
+# cp /tmp/cargo-nextest/bin/cargo-nextest out/rootfs/usr/bin/
+# fi
 
 # Build a squashed filesystem from the rootfs
 rm out/rootfs.sqsh || true
 sudo mksquashfs out/rootfs out/rootfs.sqsh -comp zstd
-bootfs_blocks="$((80000 * 2))"
+# TODO: Doubled this size due to tar saying that it ran out of space. Verify this works and maybe trim it down.
+bootfs_blocks="$((160000 * 2))"
 rootfs_bytes="$(stat --printf="%s" out/rootfs.sqsh)"
 rootfs_blocks="$((($rootfs_bytes + 512) / 512))"
 persistfs_blocks=14680064
@@ -132,6 +140,7 @@ tar xvzf out/system-boot.tar.gz -C out/bootfs
 # Replace the u-boot boot script with our own
 rm out/bootfs/boot.scr.uimg
 mkimage -T script -n "boot script" -C none -d boot.scr out/bootfs/boot.scr.uimg
+cp /usr/local/google/home/clundin/boot1900.bin out/bootfs/boot1900.bin
 umount out/bootfs
 trap cleanup1 EXIT
 
