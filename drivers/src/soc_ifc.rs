fe1ec1a14c4ed13069831aef94414d22d2858ec3
Carl Lundin <clundin@google.com>
2025-03-25 10:26:50 -0700

    Didn't work.

--- drivers/src/soc_ifc.rs
+++ drivers/src/soc_ifc.rs
@@ -150,9 +150,9 @@ impl SocIfc {
     /// * None
     pub fn flow_status_set_mailbox_flow_done(&mut self, state: bool) {
         let soc_ifc = self.soc_ifc.regs_mut();
+        let before_state = u32::from(soc_ifc.cptra_flow_status().read());
+        cprintln!("before: {:?}", before_state);
         soc_ifc.cptra_flow_status().write(|_| {
-            let before_state = u32::from(soc_ifc.cptra_flow_status().read());
-            cprintln!("before: {:?}", before_state);
             CptraFlowStatusWriteVal::from((before_state & !(1 << 31)) | (u32::from(state) << 31))
         });
         let after_state = u32::from(soc_ifc.cptra_flow_status().read());
