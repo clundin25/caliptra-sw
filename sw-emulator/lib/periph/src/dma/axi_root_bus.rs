9782595d5194be2fee2291072914c4ad3ff92903
Arthur Heymans <arthur.heymans@9elements.com>
2025-07-30 17:54:27 +0200

    feat(MCU staging): Emulate MCU staging area
    Signed-off-by: Arthur Heymans <arthur.heymans@9elements.com>

--- sw-emulator/lib/periph/src/dma/axi_root_bus.rs
+++ sw-emulator/lib/periph/src/dma/axi_root_bus.rs
@@ -31,6 +31,7 @@ use super::mci::Mci;
 
 const TEST_SRAM_SIZE: usize = 4 * 1024;
 const EXTERNAL_TEST_SRAM_SIZE: usize = 4 * 1024;
+const MCU_SRAM_SIZE: usize = 256 * 1024;
 
 pub struct AxiRootBus {
     pub reg: u32,
@@ -42,6 +43,7 @@ pub struct AxiRootBus {
     pub mci: Mci,
     sha512_acc: Sha512Accelerator,
     pub test_sram: Option<ReadWriteMemory<TEST_SRAM_SIZE>>,
+    pub mcu_sram: ReadWriteMemory<MCU_SRAM_SIZE>,
     pub indirect_fifo_status: u32,
     pub use_mcu_recovery_interface: bool,
 }
@@ -92,6 +94,7 @@ impl AxiRootBus {
         } else {
             None
         };
+        let mcu_sram = ReadWriteMemory::new();
         Self {
             reg: 0xaabbccdd,
             recovery: RecoveryRegisterInterface::new(),
@@ -101,6 +104,7 @@ impl AxiRootBus {
             event_sender: None,
             dma_result: None,
             test_sram,
+            mcu_sram,
             indirect_fifo_status: 0,
             use_mcu_recovery_interface,
         }
@@ -214,6 +218,10 @@ impl AxiRootBus {
                     return Err(LoadAccessFault);
                 }
             }
+            Self::MCU_SRAM_OFFSET..=Self::MCU_SRAM_END => {
+                let addr = (addr - Self::MCU_SRAM_OFFSET) as RvAddr;
+                return Bus::read(&mut self.mcu_sram, size, addr);
+            }
             _ => {}
         };
 
@@ -250,19 +258,24 @@ impl AxiRootBus {
                 }
             }
             Self::MCU_SRAM_OFFSET..=Self::MCU_SRAM_END => {
-                if let Some(sender) = self.event_sender.as_mut() {
-                    sender
-                        .send(Event::new(
-                            Device::CaliptraCore,
-                            Device::MCU,
-                            EventData::MemoryWrite {
-                                start_addr: (addr - Self::MCU_SRAM_OFFSET) as u32,
-                                data: val.to_le_bytes().to_vec(),
-                            },
-                        ))
-                        .unwrap();
+                if self.use_mcu_recovery_interface {
+                    if let Some(sender) = self.event_sender.as_mut() {
+                        sender
+                            .send(Event::new(
+                                Device::CaliptraCore,
+                                Device::MCU,
+                                EventData::MemoryWrite {
+                                    start_addr: (addr - Self::MCU_SRAM_OFFSET) as u32,
+                                    data: val.to_le_bytes().to_vec(),
+                                },
+                            ))
+                            .unwrap();
+                    }
+                    Ok(())
+                } else {
+                    let addr = (addr - Self::MCU_SRAM_OFFSET) as RvAddr;
+                    Bus::write(&mut self.mcu_sram, size, addr, val)
                 }
-                Ok(())
             }
             Self::OTC_FC_OFFSET..=Self::OTC_FC_END => {
                 let addr = (addr - Self::OTC_FC_OFFSET) as RvAddr;
